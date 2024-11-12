933943812fcdfabd8ea695e9caf2fe0b175152f8
Moch. Iqbal Rosyidi <iqbal.rosyidi.32@gmail.com>
2024-11-12 16:42:44 +0700

    +perhitungan SAW

---
+++ app/Http/Controllers/PerhitunganController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataSubkriteria;
+use App\Models\DataTanah;
+use App\Models\DataTanaman;
+use App\Models\DataKriteria;
+use Illuminate\Http\Request;
+
+class PerhitunganController extends Controller
+{
+    //Perhitungan SAW
+    public function index(Request $request)
+    {
+        $kriteria = DataKriteria::all();
+        $tanah = DataTanah::all();
+        // dd($request->all());
+        if ($request->all() != null) {
+            $pilihTanah = DataTanah::where('id', $request->tanah)->first();
+            $tanaman = DataTanaman::all();
+            // Data Aleternatif
+            $dataAlternatif = $this->convert($tanaman, $pilihTanah->kondisiTanah);
+
+            // Data Normalisasi
+            $normalisasi = $this->normalisasi($dataAlternatif, $kriteria);
+
+            // Data Preferensi
+            $preferensi = $this->nilaiPreferensi($normalisasi, $tanaman);
+
+            return view('perhitungan.index', compact(['kriteria', 'tanah', 'pilihTanah', 'tanaman', 'dataAlternatif', 'normalisasi', 'preferensi']));
+        }else{
+            return view('perhitungan.index', compact(['kriteria', 'tanah']));
+        }
+    }
+
+    private function convert($tanaman, $kondisiTanah) 
+    {
+        $tanaman = DataTanaman::all();
+        $result = [];
+        foreach ($tanaman as $index => $T) {
+            foreach ($kondisiTanah as $key => $kriteriaTanah) {
+                $subkriteria = $T->subkriteria()->where('id_kriteria', $kriteriaTanah->id_kriteria)->get();
+                $alternatif = [
+                    'id_kriteria' => $kriteriaTanah->id_kriteria,
+                    'id_tanaman' => $T->id,
+                    'type' => $kriteriaTanah->kriteria->type,
+                    'nilai' => $kriteriaTanah->nilai,
+                    'nama_tanaman' => $T->nama_tanaman,
+                    'range' => $T->subkriteria()->where('id_kriteria', $kriteriaTanah->id_kriteria)->pluck('range')->toArray(),
+                    'bobot' => $this->convertBobot($subkriteria, $kriteriaTanah->nilai)
+                ];
+                // dump($alternatif);
+                array_push($result, $alternatif);
+            }
+        }
+        // dd();
+        return $result;
+    }
+
+    private function convertBobot($subkriteria, $nilai) {
+        // dd(6.5 <= 6.7 && 6.7 <= 7.5 );
+        // dd($subkriteria);
+        $bobot = null;
+        foreach ($subkriteria as $sub) {
+            if (strpos($sub->range, "<") !== false) {
+                $sub->range = (double) str_replace("<", "", $sub->range);
+                if ($nilai < $sub->range) {
+                    // return $sub->kesesuaian->bobot;
+                    // return $sub->range;
+                    $bobot = $sub->kesesuaian->bobot;
+                    break;
+                }
+            }elseif (strpos($sub->range, ">") !== false) {
+                $sub->range = (double) str_replace(">", "", $sub->range);
+                if ($nilai > $sub->range) {
+                    // return $sub->kesesuaian->bobot;
+                    // return $sub->range;
+                    $bobot = $sub->kesesuaian->bobot;
+                    break;
+                }
+                break;
+            }elseif (strpos($sub->range, "-") !== false) {
+                // Mengambil nilai min dan max dari rentang
+                $range = explode('-', $sub->range);
+                $min = (double) $range[0];
+                $max = (double) $range[1];
+                // print_r($min, $max);
+                // dd();
+        
+                // Menangani anomali jika min > max
+                if ($min > $max) {
+                    $a = $max;
+                    $max = $min;
+                    $min = $a;
+                }
+                // return $min;
+                
+                if ($min <= $nilai && $nilai <= $max) {
+                    // $bobot = [$min, $max];  
+                    // return $sub->kesesuaian->bobot;
+                    // return $range;
+                    $bobot = $sub->kesesuaian->bobot;
+                    break;
+                }
+            }
+        }
+        return $bobot;
+    }
+    
+    private function normalisasi($dataAlternatif, $kriteria) {
+        // dd($dataAlternatif);
+        // Looping setiap kriteria
+        foreach ($kriteria as $C) {
+            // mencari nilai maximum tiap kriteria
+            $max[$C->id] = collect($dataAlternatif)->where('id_kriteria', $C->id)->max('bobot');
+            // mencari nilai minimum tiap kriteria
+            $min[$C->id] = collect($dataAlternatif)->where('id_kriteria', $C->id)->min('bobot');
+        }
+
+        // mencari nilai normalisasi
+        foreach ($dataAlternatif as $key => $alternatif) {
+            // cek benefit atau cost
+            $normalisasi = 0;
+            if ($alternatif['type'] == 'Benefit' || $alternatif['type'] == 'benefit') {
+                $normalisasi = $alternatif['bobot'] / $max[$alternatif['id_kriteria']];
+            }else if ($alternatif['type'] == 'Cost' || $alternatif['type'] == 'cost') {
+                $normalisasi = $alternatif['bobot'] / $min[$alternatif['id_kriteria']];
+            }
+            $result[$key] = [
+                'id_kriteria' => $alternatif['id_kriteria'],
+                'id_tanaman' => $alternatif['id_tanaman'],
+                'normalisasi' => $normalisasi,
+                'bobot_kriteria' => $kriteria->where('id', $alternatif['id_kriteria'])->first()['bobot'] / 100
+            ];
+        }
+        // mengembalikan hasil perhitungan normalisasi
+        return $result;
+    }
+
+    private function nilaiPreferensi($normalisasi, $tanaman) {
+        $normalisasi = collect($normalisasi)->groupBy('id_tanaman');
+        $result = [];
+        // mengalikan bobot dengan hasil normalisasi untuk mendapatkan nilai preferensi
+        foreach ($normalisasi as $key => $normal) {
+            foreach ($normal as $k => $N) {
+                // cara menggunakan forech yg tepat untuk method collect() karena dgn tanda panah tidak dapat dijalankan
+                $butir_preferensi[$k] = $N['bobot_kriteria'] * $N['normalisasi'];
+                $id_tanaman = $N['id_tanaman'];
+            }
+            // Hitung nilai preferensi
+            $nilai_preferensi = array_sum($butir_preferensi);
+
+            $kriteria = '';
+            if (0.75 <= $nilai_preferensi && $nilai_preferensi <= 1) {
+                $kriteria = 'S1';
+            }elseif (0.5 <= $nilai_preferensi && $nilai_preferensi < 0.75) {
+                $kriteria = 'S2';
+            }elseif (0.25 <= $nilai_preferensi && $nilai_preferensi < 0.5) {
+                $kriteria = 'S3';
+            }elseif (0 <= $nilai_preferensi && $nilai_preferensi < 0.25) {
+                $kriteria = 'N';
+            }
+            // data hasil preferensi
+            $preferensi = [
+                'nilai_preferensi' => $nilai_preferensi,
+                'id_tanaman' => $id_tanaman,
+                'nama_tanaman' => $tanaman->where('id', $id_tanaman)->first()['nama_tanaman'],
+                'kriteria' => $kriteria
+            ];
+            array_push($result, $preferensi);
+        }
+        return $result;
+    }
+}
---
+++ resources/views/perhitungan/index.blade.php
@@ -0,0 +1,131 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Pilih Tahun Penilaian Evaluasi</h5>
+
+        <form method="post" action="{{ route('perhitungan.index') }}">
+            @csrf
+            @method("GET")
+
+            <div class="row mb-3">
+                <label for="bulan" class="col-md-4 col-lg-3 col-form-label">Pilih Tanah</label>
+                <div class="col-md-8 col-lg-9">
+                    <select name="tanah" id="tanah" class="form-control">
+                        <option disabled selected>pilih tanah</option>
+                        @foreach ($tanah as $t)
+                            <option value="{{ $t->id }}">{{ $t->kode_tanah . ', '. $t->lokasi_tanah }}</option>
+                        @endforeach
+                    </select>
+
+                    <!-- <input type="number" class="form-control" name="tahun" placeholder="YYYY" min="1999" max="{{date('Y')}}"> -->
+                </div>
+            </div>
+            <div class="row mb-3 text-center">
+                <div class="col-sm-12">
+                    <button type="submit" style="width: 200px" class="btn btn-primary">Cari Penilaian</button>
+                    @if (!empty($tampil_preferensi))
+                        <a href="#" class="btn btn-warning" target="_blank">Cetak PDF</a>
+                    @endif
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h4 class="card-title">Perhitungan SAW</h4>
+    </div>
+    <div class="card-body mt-3">
+        <h5>Data Tanah</h5>
+        @if (isset($pilihTanah))
+        <div class="row">
+            <div class="col-2">Kode Tanah</div>
+            <div class="col-auto">:</div>
+            <div class="col-auto">{{ $pilihTanah->kode_tanah }}</div>
+        </div>
+        <div class="row">
+            <div class="col-2">Lokasi Tanah</div>
+            <div class="col-auto">:</div>
+            <div class="col-auto">{{ $pilihTanah->lokasi_tanah }}</div>
+        </div>
+        @foreach ($kriteria as $C)
+        @php
+            $rincianTanah = $pilihTanah->kondisiTanah()->where('id_kriteria', $C->id)->first();
+        @endphp
+        <div class="row">
+            <div class="col-2">{{$rincianTanah->kriteria->kode_kriteria}}</div>
+            <div class="col-auto">:</div>
+            <div class="col-auto">{{ $rincianTanah->nilai ?? 'Tidak Ada Data' }}</div>
+        </div>
+        @endforeach
+
+        <hr>
+        <h5>Data Alternatif</h5>
+        <table class="table table-hover table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>Nama Tanaman</th>
+                    @foreach ($kriteria as $C)
+                    <th>{{ $C->kode_kriteria }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tanaman as $T)
+                <tr>
+                    <td>{{ $T->nama_tanaman }}</td>
+                    @foreach ($kriteria as $C)
+                    <td>{{collect($dataAlternatif)->where('id_tanaman', $T->id)->where('id_kriteria', $C->id)->first()['bobot'] ?? 'Tidak Ada Data'}}</td>
+                    @endforeach
+                @endforeach
+            </tbody>
+        </table>
+        <hr>
+        <h5>Data Normalisasi</h5>
+        <table class="table table-hover table-bordered table-striped">
+        <thead class="table-dark">
+                <tr>
+                    <th>Nama Tanaman</th>
+                    @foreach ($kriteria as $C)
+                    <th>{{ $C->kode_kriteria }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tanaman as $T)
+                <tr>
+                    <td>{{ $T->nama_tanaman }}</td>
+                    @foreach ($kriteria as $C)
+                    <td>{{round(collect($normalisasi)->where('id_tanaman', $T->id)->where('id_kriteria', $C->id)->first()['normalisasi'], 3) ?? 'Tidak Ada Data'}}</td>
+                    @endforeach
+                @endforeach
+            </tbody>
+        </table>
+
+        <hr>
+        <h5>Nilai Preferensi</h5>
+        <table class="table table-hover table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>Nama Tanaman</th>
+                    <th>Preferensi</th>
+                    <th>Kriteria</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($preferensi as $P)
+                <tr>
+                    <td>{{ $P['nama_tanaman'] }}</td>
+                    <td>{{round($P['nilai_preferensi'], 3) ?? 'Tidak Ada Data'}}</td>
+                    <td>{{ $P['kriteria'] ?? 'Tidak Ada Data'}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endif
+    </div>
+</div>
+@endsection
